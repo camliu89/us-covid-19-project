1e46b726d4e5bb5d5b151f21fb53c00e06cd512e
Alexander Cam Liu <acamliu89@gmail.com>
2021-01-05 00:27:30 -0600

    add attribution to The COVID Tracking Project

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,6 +10,9 @@ type ComponentProps = {
 
 const StyledHeader = styled.div`
   padding: 1.5rem 3rem;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
   h1 {
     margin: 0;
     a {
@@ -17,15 +20,39 @@ const StyledHeader = styled.div`
       text-decoration: none;
     }
   }
+  .info-attribute {
+    border: 2px solid #0077b6;
+    padding: 1rem;
+    width: 25%;
+    line-height: 24px;
+    a {
+      font-weight: 900;
+      color: ${colors.starblue};
+      text-decoration: underline;
+    }
+    @media screen and (max-width: 600px) {
+      margin-top: 1rem;
+      width: 100%;
+    }
+  }
 `
 StyledHeader.displayName = 'StyledHeader'
 
 const Header: React.FC<ComponentProps> = ({ siteTitle }) => (
   <StyledHeader>
-    <div>
-      <h1>
-        <Link to="/">{siteTitle}</Link>
-      </h1>
+    <h1>
+      <Link to="/">{siteTitle}</Link>
+    </h1>
+    <div className="info-attribute">
+      The data is attributed to the{' '}
+      <a href="https://covidtracking.com/" target="__blank">
+        The COVID Tracking Project at The Atlantic
+      </a>{' '}
+      under the{' '}
+      <a href="https://creativecommons.org/licenses/by/4.0/" target="__blank">
+        Creative Commons CC BY 4.0 license
+      </a>
+      .
     </div>
   </StyledHeader>
 )
